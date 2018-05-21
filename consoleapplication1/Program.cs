f7b7c9cd2f8edcaf5ad1bde0d8bf0ab82b66b6b2
Kostya Pechenkin <kostya-kap@mail.ru>
2018-05-21 13:28:25 +0300

    complete

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Сколько лет?");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Тебе " + age);
+
+            Console.WriteLine("Код товара: 1\nНазвание товара: Балтика 7\nНоменклатура: 1\nОбъем: 0,5\nПроизводитель: Балтика продакшн\n" +
+                "Цена: 70\n");
+            Console.ReadLine();
+            //Console.WriteLine("Сколько лет?");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Тебе " + age);
         }
+        
     }
 }
